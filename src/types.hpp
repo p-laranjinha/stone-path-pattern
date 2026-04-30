f30e95e9ca84373b1cd8e4fa49a0ab903f966144
p-laranjinha <plcasimiro2000@gmail.com>
2026-05-01 00:01:27 +0100

    Improved type comments.

--- src/types.hpp
+++ src/types.hpp
@@ -16,19 +16,23 @@ using Edge = array<Point, 2>;
 // The 3 vertices that make a triangle.
 using Triangle = array<Point, 3>;
 
+// Hint: map<Edge, vector<CENTER>>.
 // These edges are used to draw the wire, and the vector of centers is used to
 //  help in implementing the dual mesh process.
 using PatternWire = map<Edge, vector<Point>>;
 
+// Hint: map<CENTER, map<VERTEX, array<ADJACENT_VERTEX, 2>>>.
 // This is a mid-process data structure used to save all vertices belonging to a
 //  polygon and their adjacent vertices.
 // This should only require being used during the dual mesh process.
 using PatternPrePreFill = map<Point, map<Point, array<Point, 2>>>;
 
+// Hint: map<CENTER, Polyline>.
 // This is a mid-process data structure used to save a polygon's full wire.
 // This will be used to triangulate the polygon.
 using PatternPreFill = map<Point, Polyline>;
 
+// Hint: map<CENTER, vector<Triangle>>.
 // This data structure contains a polygon's triangle partitions formed after
 //  triangulation, and is used to draw the polygon's area/inside/fill.
 using PatternFill = map<Point, vector<Triangle>>;
