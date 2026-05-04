f1cd726c517713630ada461be6446ad5f4a2a4b8
p-laranjinha <plcasimiro2000@gmail.com>
2026-05-04 21:34:16 +0100

    Added color pickers.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "patterns/triangle_grid.hpp"
 #include "types.hpp"
 #include "utils.hpp"
+#include <format>
 #include <functional>
 
 #define RAYGUI_IMPLEMENTATION
@@ -32,6 +33,8 @@ int pattern_scroll_index = 0;
 bool show_original_pattern = false;
 bool hide_pattern = false;
 bool highlight_boundary = false;
+Color base_color = raylib::Color(169, 169, 169);
+Color hover_color = raylib::Color(220, 120, 45);
 
 float gui_start_x;
 string pattern_choices;
@@ -65,20 +68,20 @@ int main() {
                                     float(get<0>(right_diagonal_chance)) / 100);
        },
        [&]() -> void {
-         GuiLabel({gui_start_x, gui_padding + text_size * 21,
+         GuiLabel({gui_start_x, gui_padding + text_size * 30,
                    gui_input_width / 2 - gui_spacing / 2, text_size},
                   "Diag. chance:");
          DrawIntInput(diagonal_chance,
-                      {gui_start_x, gui_padding + text_size * 22,
+                      {gui_start_x, gui_padding + text_size * 31,
                        gui_input_width / 2 - gui_spacing / 2, text_size},
                       0, 100);
          GuiLabel({gui_start_x + gui_input_width / 2 + gui_spacing / 2,
-                   gui_padding + text_size * 21,
+                   gui_padding + text_size * 30,
                    gui_input_width / 2 - gui_spacing / 2, text_size},
                   "Right d. chance:");
          DrawIntInput(right_diagonal_chance,
                       {gui_start_x + gui_input_width / 2 + gui_spacing / 2,
-                       gui_padding + text_size * 22,
+                       gui_padding + text_size * 31,
                        gui_input_width / 2 - gui_spacing / 2, text_size},
                       0, 100);
        }},
@@ -145,8 +148,7 @@ void DrawPattern() {
     PatternPreFill polylines = wireToPolylines(wire);
     PatternFill fill = polylinesTriangulation(polylines);
     DrawFill(fill, get<0>(max_x), get<0>(max_y), pattern_width, pattern_height,
-             padding, padding, raylib::Color::DarkBlue(),
-             raylib::Color::Orange());
+             padding, padding, base_color, hover_color);
     DrawEdges(wire, get<0>(max_x), get<0>(max_y), get<0>(thickness),
               pattern_width, pattern_height, padding, padding,
               raylib::Color(GuiGetStyle(DEFAULT, BORDER_COLOR_FOCUSED)));
@@ -159,8 +161,7 @@ void DrawPattern() {
       PatternPreFill polylines = wireToPolylines(dual_with_boundary);
       PatternFill fill = polylinesTriangulation(polylines);
       DrawFill(fill, get<0>(max_x), get<0>(max_y), pattern_width,
-               pattern_height, padding, padding, raylib::Color::DarkBlue(),
-               raylib::Color::Orange());
+               pattern_height, padding, padding, base_color, hover_color);
       if (show_original_pattern) {
         DrawEdges(wire, get<0>(max_x), get<0>(max_y), get<0>(thickness),
                   pattern_width, pattern_height, padding, padding,
@@ -175,8 +176,7 @@ void DrawPattern() {
       PatternPreFill polylines = wireToPolylines(dual);
       PatternFill fill = polylinesTriangulation(polylines);
       DrawFill(fill, get<0>(max_x), get<0>(max_y), pattern_width,
-               pattern_height, padding, padding, raylib::Color::DarkBlue(),
-               raylib::Color::Orange());
+               pattern_height, padding, padding, base_color, hover_color);
       if (show_original_pattern) {
         DrawEdges(wire, get<0>(max_x), get<0>(max_y), get<0>(thickness),
                   pattern_width, pattern_height, padding, padding,
@@ -245,7 +245,7 @@ void DrawDefaultGUI() {
   GuiLabel({gui_start_x + gui_input_width / 2 + gui_spacing / 2,
             gui_padding + text_size * 9, gui_input_width / 2 - gui_spacing / 2,
             text_size},
-           "Center offset * 100:");
+           "Max point offset * 100:");
   DrawIntInput(center_max_offset,
                {gui_start_x + gui_input_width / 2 + gui_spacing / 2,
                 gui_padding + text_size * 10,
@@ -254,8 +254,29 @@ void DrawDefaultGUI() {
 
   GuiLabel(
       {gui_start_x, gui_padding + text_size * 12, gui_input_width, text_size},
+      "Base color:");
+  GuiColorPicker(
+      {gui_start_x, gui_padding + text_size * 13, text_size * 7, text_size * 7},
+      "", &base_color);
+  GuiLabel(
+      {gui_start_x, gui_padding + text_size * 19, gui_input_width, text_size},
+      format("{},{},{}", base_color.r, base_color.g, base_color.b).c_str());
+
+  GuiLabel({gui_start_x + gui_input_width / 2, gui_padding + text_size * 12,
+            gui_input_width, text_size},
+           "Hover color:");
+  GuiColorPicker({gui_start_x + gui_input_width / 2,
+                  gui_padding + text_size * 13, text_size * 7, text_size * 7},
+                 "", &hover_color);
+  GuiLabel(
+      {gui_start_x + gui_input_width / 2, gui_padding + text_size * 19,
+       gui_input_width, text_size},
+      format("{},{},{}", hover_color.r, hover_color.g, hover_color.b).c_str());
+
+  GuiLabel(
+      {gui_start_x, gui_padding + text_size * 21, gui_input_width, text_size},
       "Pattern:");
-  GuiListView({gui_start_x, gui_padding + text_size * 13, gui_input_width,
-               float(text_size * 7)},
+  GuiListView({gui_start_x, gui_padding + text_size * 22, gui_input_width,
+               text_size * 7},
               pattern_choices.c_str(), &pattern_scroll_index, &pattern_choice);
 }
